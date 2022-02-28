780e4823d81fd08e9694b60b48608590af0f9204
Sanjana Sridhar <sanjana.sridhar@outlook.com>
2022-02-28 11:46:39 -0800

    untested python code

--- server/controller/simulation.js
+++ server/controller/simulation.js
@@ -1,3 +1,6 @@
+import { json } from "body-parser";
+const {PythonShell} = require('python-shell');
+
 export const getSimRes = async (req, res) => {
   try {
     console.log("Fetching simulation result");
@@ -12,6 +15,28 @@ export const startSimReq = async (req, res) => {
     console.log("Start simulation");
   } catch (error) {
     // TODO: correctly handle error
+    res.status(404).json({ message: error.message });
+  }
+}
+
+export const runPython = async(req, res) => {
+  // need to pass in arguments with req probably
+  try {
+    let options = {
+      mode: 'text',
+      pythonOptions: ['-u'], // get print results in real-time
+        scriptPath: '/simulation/', //If you are having python_test.py script in same folder, then it's optional.
+      args: [req.toehold, req.bm] //An argument which can be accessed in the script using sys.argv[1]
+    };
+
+    PythonShell.run('sim_strand_displacement.py', options, function (err, result){
+      if (err) throw err;
+      // result is an array consisting of messages collected
+      //during execution of script.
+      console.log('result: ', result.toString());
+      res.send(result.toString())
+    });  
+  } catch (error) {
     res.status(404).json({ message: error.message });
   }
 }
--- server/routes/simRoutes.js
+++ server/routes/simRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { getSimRes, startSimReq } from '../controller/simulation.js';
+import { getSimRes, startSimReq, runPython } from '../controller/simulation.js';
 
 const router = express.Router();
 
 router.get('./', getSimRes);
 router.post('./', startSimReq);
+router.post('./json', runPython);
 
 export default router;
